4bb8efab362970d3cf65c97b5ea799d50271ab80
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-09-14 10:58:22 +0200

    erster upload

---
+++ lowecase.py
@@ -0,0 +1,5 @@
+# -*- coding: utf8 -*-
+print "Beispiel Lowecase"
+
+eingabe = raw_input("Hallo, bitte Schreib etwas in groß und kleinschreibung:") .lower()
+print "So sieht dein Satz in Lowecase aus: " + eingabe
